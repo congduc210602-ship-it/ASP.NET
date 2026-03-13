5209f0eb00a067cc89d94846274a1218be1769a3
congduc210602-ship-it <congduc210602@gmail.com>
2026-03-13 08:14:23 +0700

    Initial commit - Project buoi 3

---
+++ TranCongDuc_21231110517/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TranCongDuc_21231110517.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
